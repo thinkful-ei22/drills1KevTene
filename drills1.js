42efa83f0a945cf6fd2e5311fd71ce40b4ea028e
teneciajennings <jenningst82@gmail.com>
2018-06-11 15:37:19 -0500

    step completed

--- drills1.js
+++ drills1.js
@@ -1,2 +1,14 @@
 'use strict';
-console.log('Hi, my name is Kevin and I\'m 24 years old');
+
+
+function whoAmI (name, age){
+  let yearOfBirth = 2018 - age;
+  console.log('Hi, my name is Kevin and I\'m 24 years old');
+  console.log(`I was born in + ${yearOfBirth}`);
+}
+return(whoAmI);
+
+function yearOfBirth (age) {
+  let yob = 2018 - age;
+
+}
